3285e20861da2c829552889eb50091a82fcece40
atanarro <tanarro@gmail.com>
2022-05-29 13:18:18 +0200

    first version

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app">
+    <div v-if="deferredPrompt" class="text-left">
+      Install now to have this installed!
+      <button @click="dismiss">Dismiss</button>
+      <button @click="install">Install</button>
+    </div>
+    <div>
+      <h1>Sleepy or not?</h1>
+      <div>
+        <button
+          class="option"
+          :key="index"
+          v-for="(button, index) in buttons"
+          @click="button.onClick"
+          :style="{ transform: 'rotate(' + button.rotation + 'deg)' }"
+        >
+          {{ button.text }}
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
-
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      deferredPrompt: null,
+      buttons: [],
+    };
+  },
+  created() {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.deferredPrompt = e;
+    });
+    window.addEventListener("appinstalled", () => {
+      this.deferredPrompt = null;
+    });
+    const NUM_BUTTONS = 24;
+    for (let i = 1; i <= NUM_BUTTONS; i++) {
+      this.buttons.push({
+        value: false,
+        text: "👎",
+        rotation: 180,
+        onClick: this.loose,
+      });
+    }
+    const randomPosition = Math.round(Math.random() * NUM_BUTTONS);
+    this.buttons[randomPosition] = {
+      value: true,
+      text: "👍",
+      rotation: 0,
+      onClick: this.win,
+    };
+  },
+  methods: {
+    loose() {
+      alert("You are still sleepy!");
+    },
+    win() {
+      alert("Congrats! You are not sleepy");
+    },
+    async dismiss() {
+      this.deferredPrompt = null;
+    },
+    async install() {
+      this.deferredPrompt.prompt();
+    },
   },
 };
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+button.option {
+  background: transparent;
+  border: transparent;
+  font-size: 25px;
 }
 </style>
